2e67485909e089f9770cdc0270e71ecad5f05596
imran123509 <hasanimran114@gmail.com>
2023-12-30 17:52:12 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const {PORT}=require('./config/serverconfig')
+const app=express();
+
+
+app.listen(PORT, ()=>{
+    console.log(`server is connected ${PORT}`)
+})
